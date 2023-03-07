c8432c4acb6f26fc0750ef6ef06483501831046d
Tyler <tyler73750@gmail.com>
2023-03-07 00:48:31 -0600

    Changed video_sender to use ZMQ instead of ImageZMQ (This enables optimized behavior)

--- RoverCam/video_receiver.py
+++ RoverCam/video_receiver.py
@@ -8,7 +8,6 @@ import numpy as np
 # Create a socket to receive frames through
 context = zmq.Context()
 sub_socket = context.socket(zmq.SUB)
-sub_socket.setsockopt(zmq.RCVHWM, 1) # Set high water mark to 1, to only keep most recent frame
 sub_socket.setsockopt(zmq.SUBSCRIBE, b'')
 sub_socket.connect("tcp://127.0.0.1:5555")
 sub_socket.subscribe(b'')
@@ -17,14 +16,11 @@ sub_socket.subscribe(b'')
 while True:
 
     # Receive, decode, and display image
-    try:
-        md = sub_socket.recv_json()
-        jpq_buffer = sub_socket.recv()
-        img_arr = np.frombuffer(jpq_buffer, dtype=np.uint8)
-        image = cv2.imdecode(img_arr, 1)
+    md = sub_socket.recv_json()
+    jpq_buffer = sub_socket.recv()
+    img_arr = np.frombuffer(jpq_buffer, dtype=np.uint8)
+    image = cv2.imdecode(img_arr, 1)
 
-        # Display image
-        cv2.imshow("video receiver",image)
-        cv2.waitKey(1)
-    except zmq.Again:
-        continue
+    # Display image
+    cv2.imshow(f"video receiver {md['idx']}",image)
+    cv2.waitKey(1)
--- RoverCam/video_sender.py
+++ RoverCam/video_sender.py
@@ -2,20 +2,26 @@
 A Video Sender made with Image ZMQ, specified as ZMQ Publisher
 """
 import cv2
-import imagezmq
+import zmq
 import numpy as np
 from VideoStreamer import VideoStreamWidget
 import time
 
-# Create ZMQ Publisher Socket for sending videos 
-sender = imagezmq.ImageSender(connect_to='tcp://*:5555', REQ_REP=False)
+# Create ZMQ Context and Publisher Socket for sending frames
+context = zmq.Context()
+publisher = context.socket(zmq.PUB)
+publisher.bind("tcp://*:5555")
+
+# Set socket behavior
+publisher.setsockopt(zmq.SNDHWM, 1)  # Set high water mark to 1 to drop frames if queue is full
+publisher.setsockopt(zmq.SNDBUF, 1)  # Set send buffer to 1 to drop frames if queue is full
 
 # Open camera instances for capturing streams
 camera1 = VideoStreamWidget(0)  # For Windows: src=0,1,... For Linux: '/dev/video*'
-#camera2 = VideoStreamWidget(0)
+camera2 = VideoStreamWidget(1)
 time.sleep(1)                   # Let cameras bake
 
-cameraList = [camera1] #, camera2]
+cameraList = [camera1 , camera2]
 
 # Start sending images until CTRL+C
 i = 0   # Index for numbering frames
@@ -30,13 +36,19 @@ while True:
         except AttributeError:
             pass
 
-        # Add counter value to the image and send it to the queue
+        # Add counter value to the image
+        frame = cv2.resize(frame, (640, 480))
         cv2.putText(frame, str(i), (100, 150), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 255), 4)
         cv2.imshow(f"{cameraIdx} sender", frame)
         cv2.waitKey(1)
 
-        # Encode and send image
+        # Encode Message
         quality = 95
         jpg_buffer = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), quality])[1].tobytes()
-        sender.send_jpg_pubsub(str(cameraIdx), jpg_buffer)
+        
+        md = dict(idx=cameraIdx, )
+        publisher.send_json(md, zmq.SNDMORE)
+        publisher.send(jpg_buffer)
+
+        # Increment camera index
         cameraIdx = cameraIdx + 1

28a87fdcd459fbd382f12db16ed0ac7e331348d7
Leo <leo@pop-os.localdomain>
2023-04-21 13:53:35 -0500

    added a branch LOL

--- RoverCam/video_receiver.py
+++ RoverCam/video_receiver.py
@@ -1,26 +1,58 @@
-"""
-A Video (or general) receiver made as a ZMQ Subscriber socket
-"""
 import zmq
 import cv2
 import numpy as np
+import threading
 
-# Create a socket to receive frames through
-context = zmq.Context()
-sub_socket = context.socket(zmq.SUB)
-sub_socket.setsockopt(zmq.SUBSCRIBE, b'')
-sub_socket.connect("tcp://127.0.0.1:5555")
-sub_socket.subscribe(b'')
+exit_event = threading.Event()
 
-# Continously receive frames
-while True:
+def video_player():
+    global exit_event
+    # Create a socket to receive frames through
+    context = zmq.Context()
+    sub_socket = context.socket(zmq.SUB)
+    sub_socket.setsockopt(zmq.SUBSCRIBE, b'')
+    sub_socket.connect("tcp://127.0.0.1:5555")
+    sub_socket.subscribe(b'')
 
-    # Receive, decode, and display image
-    md = sub_socket.recv_json()
-    jpq_buffer = sub_socket.recv()
-    img_arr = np.frombuffer(jpq_buffer, dtype=np.uint8)
-    image = cv2.imdecode(img_arr, 1)
+    # Continously receive frames
+    while not exit_event.is_set():
 
-    # Display image
-    cv2.imshow(f"video receiver {md['idx']}",image)
-    cv2.waitKey(1)
+        # Receive, decode, and display image
+        md = sub_socket.recv_json()
+        jpq_buffer = sub_socket.recv()
+        img_arr = np.frombuffer(jpq_buffer, dtype=np.uint8)
+        image = cv2.imdecode(img_arr, 1)
+
+        # Display image
+        cv2.imshow(f"video receiver {md['idx']}",image)
+        cv2.waitKey(1)
+
+    sub_socket.close()
+    context.term()
+    cv2.destroyAllWindows()
+
+def command_sender():
+    global exit_event
+    # Create a socket to send commands through
+    context = zmq.Context()
+    push_socket = context.socket(zmq.PUSH)
+    push_socket.bind("tcp://127.0.0.1:5556")
+
+    while not exit_event.is_set():
+        message = input("Enter a command(stop): ")
+        push_socket.send_string(message)
+        if message == "stop":
+            print("Stopping video stream")
+            exit_event.set()
+            push_socket.close()
+            context.term()
+
+# Creates Threads to run the video player and command sender at the same time
+videoThread = threading.Thread(target=video_player)
+commandThread = threading.Thread(target=command_sender)
+
+videoThread.start()
+commandThread.start()
+
+videoThread.join()
+commandThread.join()
--- RoverCam/video_sender.py
+++ RoverCam/video_sender.py
@@ -7,57 +7,76 @@ import numpy as np
 from VideoStreamer import VideoStreamWidget
 import time
 import argparse
+import threading
 
-# Create arguement args
-parser = argparse.ArgumentParser(description='Video Sender')
-parser.add_argument("-s", "--source", type=int, nargs='+', default=[0], help="Source of video streams (can have multiple)")
-parser.add_argument("-i", "--ip", type=str, default="*", help="IP address to bind to")
-parser.add_argument("-p", "--port", type=int, default=5555, help="Port to bind to")
-args = parser.parse_args()
+def video_sending():
+    # Create arguement args
+    parser = argparse.ArgumentParser(description='Video Sender')
+    parser.add_argument("-s", "--source", type=int, nargs='+', default=[0], help="Source of video streams (can have multiple)")
+    parser.add_argument("-i", "--ip", type=str, default="*", help="IP address to bind to")
+    parser.add_argument("-p", "--port", type=int, default=5555, help="Port to bind to")
+    args = parser.parse_args()
 
-# Create ZMQ Context and Publisher Socket for sending frames
-context = zmq.Context()
-publisher = context.socket(zmq.PUB)
-publisher.bind("tcp://*:5555")
+    # Create ZMQ Context and Publisher Socket for sending frames
+    context = zmq.Context()
+    publisher = context.socket(zmq.PUB)
+    publisher.bind("tcp://*:5555")
 
-# Set socket behavior
-publisher.setsockopt(zmq.SNDHWM, 1)  # Set high water mark to 1 to drop frames if queue is full
-publisher.setsockopt(zmq.SNDBUF, 1)  # Set send buffer to 1 to drop frames if queue is full
+    # Set socket behavior
+    publisher.setsockopt(zmq.SNDHWM, 1)  # Set high water mark to 1 to drop frames if queue is full
+    publisher.setsockopt(zmq.SNDBUF, 1)  # Set send buffer to 1 to drop frames if queue is full
 
-# Open camera instances for capturing streams
-cameraList = [] # List of camera instances (Probably better way to store cameras)
-for i in range(len(args.source)):
-    camera = VideoStreamWidget(args.source[i])
-    cameraList.append(camera)
-print("Camera instances created")
+    # Open camera instances for capturing streams
+    cameraList = [] # List of camera instances (Probably better way to store cameras)
+    for i in range(len(args.source)):
+        camera = VideoStreamWidget(args.source[i])
+        cameraList.append(camera)
+    print("Camera instances created")
 
-# Start sending images until CTRL+C
-i = 0   # Index for numbering frames
-while True:
+    # Start sending images until CTRL+C
+    i = 0   # Index for numbering frames
+    while True:
+        
+        cameraIdx = 0   # For labeling Streams
+        for camera in cameraList:
+            
+            # Read frame from camera
+            try:
+                frame = camera.read_frame()
+            except AttributeError:
+                break
+
+            # Add counter value to the image
+            frame = cv2.resize(frame, (640, 480))
+            cv2.putText(frame, str(i), (100, 150), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 255), 4)
+            cv2.imshow(f"{cameraIdx} sender", frame)
+            cv2.waitKey(1)
+
+            # Encode Message
+            quality = 95
+            jpg_buffer = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), quality])[1].tobytes()
+            
+            md = dict(idx=cameraIdx, )
+            publisher.send_json(md, zmq.SNDMORE)
+            publisher.send(jpg_buffer)
+
+            # Increment camera index
+            cameraIdx = cameraIdx + 1
+            i = i + 1
+
+
+def command_receiver():
+    context = zmq.Context()
+    socket = context.socket(zmq.PULL)
+    socket.connect("tcp://127.0.0.1:5556")
+    message = socket.recv_string()
+    print(f"Program status: {message}")
+    if message == "stop":
+        return False
+    else:
+        return True
     
-    cameraIdx = 0   # For labeling Streams
-    for camera in cameraList:
-        
-        # Read frame from camera
-        try:
-            frame = camera.read_frame()
-        except AttributeError:
-            break
-
-        # Add counter value to the image
-        frame = cv2.resize(frame, (640, 480))
-        cv2.putText(frame, str(i), (100, 150), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 255), 4)
-        cv2.imshow(f"{cameraIdx} sender", frame)
-        cv2.waitKey(1)
-
-        # Encode Message
-        quality = 95
-        jpg_buffer = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), quality])[1].tobytes()
-        
-        md = dict(idx=cameraIdx, )
-        publisher.send_json(md, zmq.SNDMORE)
-        publisher.send(jpg_buffer)
-
-        # Increment camera index
-        cameraIdx = cameraIdx + 1
-        i = i + 1
+videoThreading = threading.Thread(target = video_sending)
+commandThreading = threading.Thread(target = command_receiver)      
+videoThreading.start()
+commandThreading.start()
